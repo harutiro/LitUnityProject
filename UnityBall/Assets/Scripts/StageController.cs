7b184a5f2aca82a2b2cee1a9b6e345dc0f73e39d
harutiro <harutiro2027@outlook.jp>
2023-09-03 04:44:57 +0900

    unityball

---
+++ UnityBall/Assets/Scripts/StageController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+
+	public GameObject player;
+	Vector3 pivotDirection;
+	public float rotateSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		pivotDirection = new Vector3(0, 0, 1);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+			// 回転する軸の中心、回転する軸、回転する速度
+			transform.RotateAround(player.transform.position, pivotDirection, -rotateSpeed);
+        }
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.RotateAround(player.transform.position, pivotDirection, rotateSpeed);
+
+		}
+	}
+}
